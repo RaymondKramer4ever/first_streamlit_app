01fdfbacb893eef2bba5236066f48a1efc983655
RaymondKramer4ever <134390684+RaymondKramer4ever@users.noreply.github.com>
2023-05-26 10:12:24 +0200

    Update streamlit_app.py
    added new text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# New Section to display fruityvice api response
+fruit_choice = streamlit.text_input('What kind of fruit would you like to add?', 'Jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
+
 
 
