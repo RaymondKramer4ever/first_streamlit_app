c209decc79daf95f102aa4c9473656a0811d65fa
RaymondKramer4ever <134390684+RaymondKramer4ever@users.noreply.github.com>
2023-05-26 12:53:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,7 +71,7 @@ if streamlit.button('Add a fruit to the list'):
    back_from_function = insert_row_snowflake(add_my_fruit)
    streamlit.text(back_from_function)
 
-streamlit.write
+streamlit.write()
 
 
 
